d7064b6d8d82a5281bdccfa98d6de9a2803c21b5
fungmo <mofengfs@gmail.com>
2021-05-11 11:46:50 +0800

    paper theme init

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import useCachedResources from './hooks/useCachedResources';
+import Navigation from './navigation';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import merge from 'deepmerge';
+
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <PaperProvider theme={CombinedDarkTheme}>
+        <Navigation theme={CombinedDefaultTheme} />
+    </PaperProvider>
+    );
+  }
+}
